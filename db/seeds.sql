731c50e6cdcdea0606edb024b4007c847f819ea6
Zachary Ferreira <Pearlybrah@github.com>
2019-03-13 02:12:48 -0700

    trial2

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Double Double with Cheese", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bison  Burger with Applewood Smoked Bacon", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("BigMac", false);
